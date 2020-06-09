12c9c5c99e203a0276e6914db9aebcf07576ded0
Prince Dev <princedev432@gmail.com>
2020-06-09 21:27:30 +0530

    error fixed v new

--- powerbolt/src/main/java/com/mobicomly/powerbolt/XLLoader.java
+++ powerbolt/src/main/java/com/mobicomly/powerbolt/XLLoader.java
@@ -150,9 +150,4 @@ class XLLoader {
 
     public native int unInit();
 
-    public XLLoader() {
-        System.loadLibrary("xl_stat");
-        System.loadLibrary("xluagc");
-        System.loadLibrary("xl_thunder_sdk");
-    }
 }
